5100f957b19ee6732e60b5b008b97ddc9dc76f2a
Fredrik Rose <fredrik.rose.developer@gmail.com>
2022-12-06 19:50:26 +0100

    Generate the Mandelbrot set in main
    Also save the image to disc as a PGM image.

--- src/main.c
+++ src/main.c
@@ -1,44 +1,38 @@
-#include <MandelbrotSet/complex.h>
 #include <MandelbrotSet/image.h>
+#include <MandelbrotSet/mandelbrot_set.h>
 #include <MandelbrotSet/pgm.h>
 
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
+
+#define RESOLUTION_X (3840)
+#define RESOLUTION_Y (3840)
+#define DIVERGE_THRESHOLD (255u)
+#define MANDELBROT_IMAGE_PATH ("mandelbrot.pgm")
 
 int main(int argv, char *argc[])
 {
     (void)argv;
     (void)argc;
 
-    const struct CMPLX_Complex a = {
-        .real = 1.0,
-        .imag = 2.0
-    };
-
-    const struct CMPLX_Complex b = {
-        .real = 3.0,
-        .imag = 4.0
-    };
-
-    struct CMPLX_Complex c = CMPLX_add(a, b);
-    c = CMPLX_square(c);
-    const double d = CMPLX_abs(c);
-
-    printf("(%.2lf + %.2lfi), %.2lf\n", c.real, c.imag, d);
-
     struct IMG_Image image = {
-        .width = 10,
-        .height = 10,
-        .pixels = calloc(10 * 10, sizeof(*image.pixels))
+        .width = RESOLUTION_X,
+        .height = RESOLUTION_Y,
+        .pixels = calloc(RESOLUTION_X * RESOLUTION_Y, sizeof(*image.pixels))
+    };
+    const struct MBROT_Range range = {
+        .min = {
+            .real = -2.0,
+            .imag = -2.0
+        },
+        .max = {
+            .real = 2.0,
+            .imag = 2.0
+        }
     };
 
-    IMG_set_pixel(&image, 1, 2, 3U);
-    const uint16_t pixel = IMG_get_pixel(&image, 1, 2);
-
-    printf("pixel: %u\n", pixel);
-
-    PGM_save(&image, 255U, "mandelbrot.pgm");
+    MBROT_generate_mandelbrot_set(&image, &range, DIVERGE_THRESHOLD);
+    PGM_save(&image, DIVERGE_THRESHOLD, MANDELBROT_IMAGE_PATH);
 
     free(image.pixels);
 }
